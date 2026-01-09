8068625735dbbd73f27309c8cebd872881764af2
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-01-09 00:47:19 +0000

    build(api): main.go gin code added and oapi-codegen config to generate the code from the openapi spec

---
+++ be/cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/darkcl0ud/no-cors-webapp/be/internal/api"
+	"github.com/darkcl0ud/no-cors-webapp/be/internal/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	myHandler := handler.Server{}
+
+	r := gin.Default()
+
+	api.RegisterHandlers(r, myHandler)
+
+	err := r.Run(":8080")
+
+	if err != nil {
+		panic(err)
+	}
+}
